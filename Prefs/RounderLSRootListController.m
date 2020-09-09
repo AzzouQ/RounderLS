68db5cdd2703184f00e1c69187426e6272098c2f
Azzou <60717896+AzzouDuGhetto@users.noreply.github.com>
2020-09-09 19:55:36 +0200

    fix alert

--- Prefs/RounderLSRootListController.m
+++ Prefs/RounderLSRootListController.m
@@ -141,8 +141,8 @@
 - (void)reset {
 
 	UIAlertController *resetAlert = [UIAlertController alertControllerWithTitle:@"RounderLS" message:@"Do you really want to reset your preferences ?" preferredStyle:UIAlertControllerStyleActionSheet];
-	UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Nah" style:UIAlertActionStyleCancel handler:nil];	
-	UIAlertAction *confirmAction = [UIAlertAction actionWithTitle:@"Yeah" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *action) {
+	UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"No" style:UIAlertActionStyleCancel handler:nil];	
+	UIAlertAction *confirmAction = [UIAlertAction actionWithTitle:@"Yes" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *action) {
 		for (NSString *key in [prefs dictionaryRepresentation]) {
 			[prefs removeObjectForKey:key];
 		}
